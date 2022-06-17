8de62f06cd6acfb0bed54b50541aa2e88df481ae
Yashwanth K <yashwanthkkn@gmail.com>
2022-06-17 13:43:39 +0530

    added room and char

---
+++ repository/room.repo.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+AWS.config.loadFromPath('./config.json');
+
+
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'gh-room';
