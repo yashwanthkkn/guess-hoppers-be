8b0272c88258328cfe79760a6267b0ab9ef92498
Yashwanth K <yashwanthkkn@gmail.com>
2022-06-21 10:27:45 +0530

    added join room event

--- index.js
+++ index.js
@@ -5,14 +5,63 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-const routes = require('./lib/controllers')
+const io = new Server(server,{
+  cors : {
+    origin : '*'
+  }
+});
+const routes = require('./lib/controllers');
+const { User } = require('./lib/repository/user.repo');
 
 app.use(bodyParser.json())
 app.use(routes)
 
+const formatMessage = (username, message)=>{
+  return {username, message}
+}
+
+const botName = "GameBot"
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  // console.log(socket.id, socket.rooms);
+  console.log("User connected");
+
+  socket.on('joinGame', ({roomId,user}) => {
+    
+    socket.join(roomId) 
+
+    socket.emit('message', formatMessage(botName,"Welcome to the Game"))
+
+    socket.broadcast
+      .to(roomId)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the Game`)
+      );
+
+    // get already existing users using http req
+    // use this to get new users
+    io.to(roomId).emit('newUser', {
+      user: user
+    });
+  })
+
+  socket.on('guess', ({roomId, userId, guess}) => {
+    // Todo
+  })
+
+  socket.on('message', ({roomId,userName, message}) => {
+    socket.broadcast
+      .to(roomId)
+      .emit(
+        'message',
+        formatMessage(userName, message)
+      );
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log("User disconnected", reason)
+  });
 });
 
 server.listen(3000, () => {
